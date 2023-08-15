a2999aa694bdc4a3044821e6335a04385a40faac
Gabriela <gabrielavs1226@gmail.com>
2023-08-15 18:29:48 -0300

    feat: filter bar

--- src/components/organisms/MoviesList/MoviesList.jsx
+++ src/components/organisms/MoviesList/MoviesList.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import { Link, useSearchParams } from "react-router-dom";
-import { useQuery, useQueryClient,} from "@tanstack/react-query";
 import Select from 'react-select';
 import axios from 'axios';
 import ContentCard from "../../molecules/ContentCard/ContentCard";
@@ -9,38 +8,47 @@ import ContentCard from "../../molecules/ContentCard/ContentCard";
 import '../../../styles/main.scss'
 
 const options = [
-    { value: 'popularity.asc', label: 'Popularidade (crescente)' },
-    { value: 'release_date.desc', label: 'Data de lançamento (decrescente)' },
-    { value: 'release_date.asc', label: 'Data de lançamento (crescente)' },
-    { value: 'vote_average.desc', label: 'Avaliação dos usuários (decrescente)' },
-    { value: 'vote_average.asc', label: 'Avaliação dos usuários (crescente)' },
-    { value: 'original_title.asc', label: 'Título (A-Z)' },
-    { value: 'original_title.desc', label: 'Título (Z-A)' },
-  ];
+    { value: 'popularity', label: 'Filmes mais populares' },
+    { value: 'cinema', label: 'Últimos lançamentos nos cinemas' },
+    { value: 'vote_average', label: 'Melhor avaliados' },
+  ];  
 
 export default function MoviesList() {
-    const queryClient = useQueryClient()
-    const [searchParams, setSearchParams] = useSearchParams()
+    const [searchParams] = useSearchParams()
     const [page, setPage] = useState(1); 
+    const [movies, setMovies] = useState([]);
     const [selectedOption, setSelectedOption] = useState(null);
+    const [genre, setGenre] = useState("")
 
-    // const genreFilter = searchParams.get("genre_ids")
-    // console.log(genreFilter)
+    useEffect(() => {
+        if(!selectedOption){
+            fetchMovieData(page)
+        }
 
-    // const displayedMovies = genreFilter
-    // ? movies.filter(movie => movie.genre_ids === genreFilter)
-    // : movies
+        if (selectedOption?.value === 'vote_average') {
+            fetchTopRatedMovies(page)
+        }
 
-    // function handleFilterChange(key, value) {
-    //     setSearchParams(prevParams => {
-    //         if (value === null) {
-    //             prevParams.delete(key)
-    //         } else {
-    //             prevParams.set(key, value)
-    //         }
-    //         return prevParams
-    //     })
-    // }
+        if (selectedOption?.value === 'cinema') {
+            fetchNowPlayingMovies(page)
+        }
+
+        if(selectedOption?.value === 'popularity'){
+            fetchMovieData(page)
+        }
+    }, [page, selectedOption])
+
+    useEffect(() => {
+    async function handleGenre() {
+        const { data } = await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=427c6a1f4842e0a377188d4ee2935509&language=pt-BR&page=${page}`)
+        const filteredMovies = data.results.filter(movie => movie.genre_ids.includes(genre))
+        setMovies(filteredMovies)
+        console.log(filteredMovies)
+    }
+    handleGenre()
+
+    },[genre])
+
 
     const handlePrevPage = () => {
         page > 1 ? setPage(page - 1) : null
@@ -50,33 +58,29 @@ export default function MoviesList() {
         setPage(page + 1)
     }
 
-    async function fetchMovieData(page = 1) {
+    async function fetchMovieData(page) {
         const { data } = await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=427c6a1f4842e0a377188d4ee2935509&language=pt-BR&page=${page}`)
-        return data?.results;
+        setMovies(data?.results);
+        return movies;
     }    
 
-    const {status, data, error, isFetching, isPreviousData, isLoading} = useQuery({
-        queryFn: () => fetchMovieData(page),
-        queryKey: ['movies', page],
-        keepPreviousData: true,
-        staleTime: 5000,
-    })
+    async function fetchTopRatedMovies(page) {
+        const { data } = await axios.get(`https://api.themoviedb.org/3/movie/top_rated?api_key=427c6a1f4842e0a377188d4ee2935509&language=pt-BR&page=${page}`)
+        setMovies(data?.results);
+        return movies;
+    }
 
-    useEffect(() => {
-        if (!isPreviousData && data?.hasMore) {
-          queryClient.prefetchQuery({
-            queryKey: ['movies', page + 1],
-            queryFn: () => fetchMovieData(page + 1),
-          })
-        }
-      }, [data, isPreviousData, page, queryClient])
-
+    async function fetchNowPlayingMovies(page) {
+        const { data } = await axios.get(`https://api.themoviedb.org/3/movie/now_playing?api_key=427c6a1f4842e0a377188d4ee2935509&language=pt-BR&page=${page}`)
+        setMovies(data?.results);
+        return movies;
+    }
 
 
     return (
         <div className="movies">
             <div className="title__container">
-                <h1 className="title">Filmes Populares</h1>
+                <h1 className="title">{selectedOption ? selectedOption.label : "Filmes mais populares"}</h1>
             </div>
             
             <div className="movies__container">
@@ -85,7 +89,7 @@ export default function MoviesList() {
                         <h3 className="sort__title">Ordenar por</h3>
                         <div className="custom-select">
                             <Select
-                                defaultValue={selectedOption}
+                                defaultValue={options[0]}
                                 onChange={setSelectedOption}
                                 options={options}
                             />
@@ -95,53 +99,43 @@ export default function MoviesList() {
                     <div className="aside__filter">
                         <h3 className="filter__title">Filtrar por</h3>
                         <h4 className="filter__subtitle">Gêneros</h4>
-                            <button className="buttons__button">Ação</button>
-                            <button className="buttons__button">Aventura</button>
-                            <button className="buttons__button">Animação</button>
-                            <button className="buttons__button">Comédia</button>
-                            <button className="buttons__button">Crime</button>
-                            <button className="buttons__button">Documentário</button>
-                            <button className="buttons__button">Drama</button>
-                            <button className="buttons__button">Família</button>
-                            <button className="buttons__button">Fantasia</button>
-                            <button className="buttons__button">História</button>
-                            <button className="buttons__button">Terror</button>
-                            <button className="buttons__button">Música</button>
-                            <button className="buttons__button">Mistério</button>
-                            <button className="buttons__button">Romance</button>
-                            <button className="buttons__button">Ficção científica</button>
-                            <button className="buttons__button">Filme para TV</button>
-                            <button className="buttons__button">Suspense</button>
-                            <button className="buttons__button">Guerra</button>
-                            <button className="buttons__button">Faroeste</button>
+                            <button className="buttons__button" onClick={() => setGenre(28)}>Ação</button>
+                            <button className="buttons__button" onClick={() => setGenre(12)}>Aventura</button>
+                            <button className="buttons__button" onClick={() => setGenre(16)}>Animação</button>
+                            <button className="buttons__button" onClick={() => setGenre(35)}>Comédia</button>
+                            <button className="buttons__button" onClick={() => setGenre(80)}>Crime</button>
+                            <button className="buttons__button" onClick={() => setGenre(99)}>Documentário</button>
+                            <button className="buttons__button" onClick={() => setGenre(18)}>Drama</button>
+                            <button className="buttons__button" onClick={() => setGenre(10751)}>Família</button>
+                            <button className="buttons__button" onClick={() => setGenre(14)}>Fantasia</button>
+                            <button className="buttons__button" onClick={() => setGenre(36)}>História</button>
+                            <button className="buttons__button" onClick={() => setGenre(27)}>Terror</button>
+                            <button className="buttons__button" onClick={() => setGenre(10402)}>Música</button>
+                            <button className="buttons__button" onClick={() => setGenre(9648)}>Mistério</button>
+                            <button className="buttons__button" onClick={() => setGenre(10749)}>Romance</button>
+                            <button className="buttons__button" onClick={() => setGenre(878)}>Ficção científica</button>
+                            <button className="buttons__button" onClick={() => setGenre(10770)}>Filme para TV</button>
+                            <button className="buttons__button" onClick={() => setGenre(53)}>Suspense</button>
+                            <button className="buttons__button" onClick={() => setGenre(10752)}>Guerra</button>
+                            <button className="buttons__button" onClick={() => setGenre(37)}>Faroeste</button>
                     </div>
                 </aside>
             <div className="container__list">
-                {/* <button
-                    onClick={() => handleFilterChange("genre_ids", 16)}
-                >16</button>
-
-                <button
-                    onClick={() => handleFilterChange("genre_ids", 28)}
-                >28</button> */}
-
-                
-
-                {data?.map(movie => (
-                    <Link 
-                        to={`/filmes/${movie.id}`} 
-                        state={{ search: `?${searchParams.toString()}` }}
-                        style={{ textDecoration: 'none', display: 'unset'}}
-                    >  
-                        <ContentCard 
-                            key={movie.id}
-                            Image={`https://image.tmdb.org/t/p/w500${movie.poster_path}` }
-                            Title={movie.title}
-                            Year={(movie.release_date).slice(0,4)}
-                            Rating={movie.vote_average}
-                        />
-                    </Link>
-                ))}
+                {movies?.map(movie => (
+                <Link key={movie.id}
+                    to={`/filmes/${movie.id}`} 
+                    state={{ search: `?${searchParams.toString()}` }}
+                    style={{ textDecoration: 'none', display: 'unset'}}
+                >  
+                    <ContentCard 
+                        key={movie.id}
+                        Image={`https://image.tmdb.org/t/p/w500${movie.poster_path}` }
+                        Title={movie.title}
+                        Year={(movie.release_date).slice(0,4)}
+                        Rating={movie.vote_average}
+                    />
+                </Link>
+            ))}
             </div> 
 
             <div className="buttons">
@@ -149,10 +143,6 @@ export default function MoviesList() {
                 <span style={{color: "white"}} className="buttons buttons__span">{page}</span>
                 <button onClick={handleNextPage}className="buttons buttons__button">Próxima página</button>
             </div>
-
-            {!isLoading && data?.length === 0 && <p>Nenhum filme encontrado</p>}
-
-            {isLoading && <p>Carregando...</p>}
             </div>
         </div>
     )
