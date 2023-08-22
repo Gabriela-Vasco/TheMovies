fdd30c0c8f2ef57222085b87f2bc58bd628a2734
Gabriela <gabrielavs1226@gmail.com>
2023-08-22 11:36:44 -0300

    updated main

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,11 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom'
 import App from './App.jsx'
 import './index.css'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
-const client = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <QueryClientProvider client={client}>
       <App />
-    </QueryClientProvider>
   </React.StrictMode>,
 )
